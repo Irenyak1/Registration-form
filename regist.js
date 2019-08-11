fea8621f1b7d92e2a645a9486b8e76cbdbf1b724
Irenyak1 <inyakate@gmail.com>
2019-08-11 20:01:34 +0300

    comments in Javascript file and refactor some html

--- regist.js
+++ regist.js
@@ -1,7 +1,9 @@
 /* 
    Function to validate all the inputs, radio buttons and check box 
    in the html file and return a valid response to help the 
-   user know where they have inputed a wrong value
+   user know where they have inputed a wrong value. If the value provided 
+   is wrong, the input will turn red and if the value is right, it will 
+   turn green.
 */
 const validate = () => {
     const myId = document.getElementById("userId");
@@ -10,10 +12,10 @@ const validate = () => {
     const selects = document.getElementById("selector");
     const myZip = document.getElementById("zip");
     const myMail = document.getElementById("mail");
-    const myChoice = document.getElementsByName("myRadios");
-    // const checks = document.getElementsByName("myChecks");
-
-
+    const checks = document.getElementsByName("myChecks");
+    
+    
+    //Validating user Id making sure it is between 5 to 12 characters.
     let IdValid = false;
     myId.addEventListener('input', (e) => {
         // Method not to drop the already typed characters.
@@ -30,6 +32,7 @@ const validate = () => {
 
     });
 
+    //Validating password to make sure it is between 7 to 12 characters.
     let passValid = false;
     pass.addEventListener('input', (elmt) => {
         elmt.preventDefault();
@@ -44,7 +47,7 @@ const validate = () => {
 
         }
     });
-
+   //Validating name making sure it contains only alphabetes.
     let namVal = false;
     nam.addEventListener('input', (elme) => {
         elme.preventDefault();
@@ -61,6 +64,7 @@ const validate = () => {
         }
     });
 
+    //Validating country selection making sure one country is selected.
     let selectValid = false;
     selects.addEventListener('input', () => {
 
@@ -74,6 +78,7 @@ const validate = () => {
         }
     });
 
+    //Validating zipcode to make sure it is only numeric.
     let zipVal = false;
     myZip.addEventListener('input', (f) => {
         f.preventDefault();
@@ -90,11 +95,12 @@ const validate = () => {
 
     });
 
+    //Validating email to make sure it follows the right email pattern.
     var emailValid = false;
     myMail.addEventListener('input', (m) => {
         m.preventDefault();
 
-        const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        const email = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         if ((myMail.value.match(email))) {
             myMail.style.border = "1px solid green";
             emailValid = true;
@@ -105,75 +111,11 @@ const validate = () => {
         }
 
     });
-    // nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-    //     for (var item = 0; item < myChoice.length; item++) {
-    //         if (myChoice[item].checked)
-    //             return true;
-
-    //     }
-    //     if (item == myChoice.length){
-    //         alert("Required. No radio button selected.");
-    //         myChoice.style.border = "1px solid green";
-    //     }
-
-// bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-    // var validateRadios = 0;
-    // myChoice.addEventListener('input', (r) => {
-    //     r.preventDefault();
-
-    //     for (var item = 0; item < myChoice.length; item++) {
-    //         if (myChoice[item].checked) {
-    //             myChoice.style.border = "1px solid green";
-    //             validateRadios = true;
-
-    //         } else {
-    //             myChoice.style.border = "1px solid red";
-    //             validateRadios = false;
-    //         }
-    //     }
-
-
-    // });
-    //nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-
-
-    var checked = false;
-	var checks = document.getElementsByName("myChecks");
-	for(var i=0; i < checks.length; i++){
-		if(checks[i].checked) {
-            checks.style.border = "1px solid green";
-			checked = true;
-		}
-	}
-	if (!checked) {
-        checks.style.border = "1px solid red";
-        checked = false;
-		
-	}
-	
-
-    // var validChecks = false;
-    // checks.addEventListener('input', (x) => {
-    //     x.preventDefault();
-
-    //     var mychecked = false;
-    //     for (var key = 0; key < checks.length; key++) {
-    //         if (checks[key].checked)
-    //             mychecked
-    //         // validChecks = true;
-    //     }else {
-
-    //     }
-
-
-
-    // });
-    //     
-    //     if (mychecked != 1) {
-    //         alert ("Required. Select one checkbox only");
-    //         checks.style.border = "1px solid green";
-    //     }
-
+        
+    /* 
+       Method to add an event listener to the form to return a valid 
+       response in case an input is not filled correctly.
+    */
     document.getElementById('the-form').addEventListener('submit', () => {
 
         if (IdValid == false) {
@@ -194,20 +136,16 @@ const validate = () => {
         }
         if (zipVal == false) {
             alert("The Zipcode is required and must be numeric only.");
+            return;
         }
         if (emailValid == false) {
             alert('Please fill in a valid email');
+            return;
         }
-        // if (validateRadios == false) {
-        //     alert("No radio button selected.");
-        // }
-        // if (checked == false){
-        //     alert("Required! Please select one checkbox only");
-        // }
-
 
     });
 
 }
 
+//Calling the validate function. 
 validate();
