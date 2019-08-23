ef23e8a44c5b2ba577bd1530040bee7b53834674
Irenyak1 <inyakate@gmail.com>
2019-08-23 15:24:44 +0300

    Refactor my js code to print alerts within the form body

--- regist.js
+++ regist.js
@@ -6,72 +6,63 @@
    turn green.
 */
 const validate = () => {
-    const myId = document.getElementById("userId");
-    const pass = document.getElementById("passw");
-    const nam = document.getElementById("nme");
-    const selects = document.getElementById("selector");
-    const myZip = document.getElementById("zip");
-    const myMail = document.getElementById("mail");
-    const checks = document.getElementsByName("myChecks");
-    
-    
     //Validating user Id making sure it is between 5 to 12 characters.
+    const myId = document.getElementById("userId");
+    let idregex = /^\w{5,12}$/;
     let IdValid = false;
-    myId.addEventListener('input', (e) => {
+    myId.addEventListener("input", e => {
         // Method not to drop the already typed characters.
         e.preventDefault();
 
-        if (myId.value.length < 5 || myId.value.length > 12) {
+        if (idregex.test(myId.value) == false) {
             myId.style.border = "1px solid red";
             IdValid = false;
-
         } else {
             myId.style.border = "1px solid green";
             IdValid = true;
         }
-
     });
 
     //Validating password to make sure it is between 7 to 12 characters.
+    const pass = document.getElementById("passw");
+    passregex = /^\w{7,12}$/;
     let passValid = false;
-    pass.addEventListener('input', (elmt) => {
-        elmt.preventDefault();
+    pass.addEventListener("input", e => {
+        e.preventDefault();
 
-        if (pass.value.length < 7 || pass.value.length > 12) {
+        if (passregex.test(pass) == false) {
             pass.style.border = "1px solid red";
             passValid = false;
-
         } else {
             pass.style.border = "1px solid green";
             passValid = true;
-
         }
     });
-   //Validating name making sure it contains only alphabetes.
-    let namVal = false;
-    nam.addEventListener('input', (elme) => {
-        elme.preventDefault();
 
-        const myName = (/^[A-Za-z]+$/);
-        if ((nam.value.match(myName))) {
+    //Validating name making sure it contains only alphabetes.
+    const nam = document.getElementById("nme");
+    const myName = /^[A-Za-z]+$/;
+    let namVal = false;
+    nam.addEventListener("input", e => {
+        e.preventDefault();
+
+        if (nam.value.match(myName)) {
             nam.style.border = "1px solid green";
             namVal = true;
-
         } else {
             nam.style.border = "1px solid red";
             namVal = false;
-
         }
     });
 
     //Validating country selection making sure one country is selected.
+    const selects = document.getElementById("selector");
+    // const sel = document.getElementById("selct");
     let selectValid = false;
-    selects.addEventListener('input', () => {
-
+    selects.addEventListener("input", () => {
         if (selects.value) {
             selects.style.border = "1px solid green";
             selectValid = true;
-
         } else {
             selects.style.border = "1px solid red";
             selectValid = false;
@@ -79,73 +70,152 @@ const validate = () => {
     });
 
     //Validating zipcode to make sure it is only numeric.
+    const myZip = document.getElementById("zip");
+    const zipcod = /^[0-9]+$/;
     let zipVal = false;
-    myZip.addEventListener('input', (f) => {
-        f.preventDefault();
+    myZip.addEventListener("input", e => {
+        e.preventDefault();
 
-        const zipcod = (/^[0-9]+$/);
-        if ((myZip.value.match(zipcod))) {
+        if (myZip.value.match(zipcod)) {
             myZip.style.border = "1px solid green";
             zipVal = true;
-
         } else {
             myZip.style.border = "1px solid red";
             zipVal = false;
         }
-
     });
 
-    //Validating email to make sure it follows the right email pattern.
+    // Validating email to make sure it follows the right email pattern.
+    const myMail = document.getElementById("mail");
+    const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var emailValid = false;
-    myMail.addEventListener('input', (m) => {
-        m.preventDefault();
+    myMail.addEventListener("input", e => {
+        e.preventDefault();
 
-        const email = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        if ((myMail.value.match(email))) {
-            myMail.style.border = "1px solid green";
-            emailValid = true;
-
-        } else {
+        if (emailregex.test(myMail.value) == false) {
             myMail.style.border = "1px solid red";
             emailValid = false;
+        } else {
+            myMail.style.border = "1px solid green";
+            emailValid = true;
         }
-
     });
-        
+
     /* 
-       Method to add an event listener to the form to return a valid 
-       response in case an input is not filled correctly.
+        Getting checkbox input fields from the documents by their ids and
+        assigning them to variables eng and nonEng.
     */
-    document.getElementById('the-form').addEventListener('submit', () => {
+    const eng = document.getElementById("eng");
+    const nonEng = document.getElementById("non-eng");
+
+    //An event listener triggered when the english checkbox is checked
+    eng.addEventListener("change", () => {
+        //This checks if the english checkbox is checked and if so then the
+        //non english checkbox will be disabled.
+        if (eng.checked == true) {
+            nonEng.checked = false;
+        }
+    });
+
+    //An event listener triggered when the non-english checkbox is checked
+    nonEng.addEventListener("change", () => {
+        // This checks if the nonenglish checkbox is checked and if so then the
+        //english checkbox will be disabled.
+        if (nonEng.checked == true) {
+            eng.checked = false;
+        }
+    });
+
+    /* 
+        Method to add an event listener to the submit button of the form to return a valid 
+        response in case an input is not filled correctly.
+    */
+    
+    document.getElementById("the-form").addEventListener("submit", e => {
+        e.preventDefault();
 
         if (IdValid == false) {
-            alert('User Id is required and must be of length 5 to 12.');
-            return;
+            document.getElementsByClassName("message")[0].innerHTML =
+                "Required and must be of length 5 to 12.";
+            document.getElementsByClassName("message")[0].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[0].innerHTML = "";
         }
+
         if (passValid == false) {
-            alert('Password is required and must be of length 7 to 12.');
-            return;
+            document.getElementsByClassName("message")[1].innerHTML =
+                "Required and must be of length 7 to 12.";
+            document.getElementsByClassName("message")[1].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[1].innerHTML = "";
         }
+
         if (namVal == false) {
-            alert('Name is required and must be alphabetes only.');
-            return;
+            document.getElementsByClassName("message")[2].innerHTML =
+                "Required and alphabetes only.";
+            document.getElementsByClassName("message")[2].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[2].innerHTML = "";
         }
+
         if (selectValid == false) {
-            alert("You must select a country.");
-            return;
+            document.getElementsByClassName("message")[3].innerHTML =
+                "Required.Must select a country.";
+            document.getElementsByClassName("message")[3].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[3].innerHTML = "";
         }
+
         if (zipVal == false) {
-            alert("The Zipcode is required and must be numeric only.");
-            return;
+            document.getElementsByClassName("message")[4].innerHTML =
+                "Required.Must be numeric only.";
+            document.getElementsByClassName("message")[4].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[4].innerHTML = "";
         }
+
         if (emailValid == false) {
-            alert('Please fill in a valid email');
-            return;
+            document.getElementsByClassName("message")[5].innerHTML =
+                "Required.Must be a valid email.";
+            document.getElementsByClassName("message")[5].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif;";
+        } else {
+            document.getElementsByClassName("message")[5].innerHTML = "";
         }
 
+        /* 
+            Getting radio button input fields from the document by their ids and 
+            assigning them to variables male and female.
+        */
+        const male = document.getElementById("mal");
+        const female = document.getElementById("femal");
+        //Checking if a radio button is selected or none
+        if (male.checked == false && female.checked == false) {
+            document.getElementsByClassName("message")[6].innerHTML = "Required.";
+            document.getElementsByClassName("message")[6].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif; padding-left: 79px;";
+        } else {
+            document.getElementsByClassName("message")[6].innerHTML = "";
+        }
+
+        /* 
+            This loops through the group checkboxes to check if both of them are not checked and
+            if both are not checked then a message is return to select one of them 
+        */
+        if (nonEng.checked == false && eng.checked == false) {
+            document.getElementsByClassName("message")[7].innerHTML = "Required.";
+            document.getElementsByClassName("message")[7].style =
+                "color: red; font-family:Arial, Helvetica, sans-serif; padding-left: 30px;";
+        } else {
+            document.getElementsByClassName("message")[7].innerHTML = "";
+        }
     });
+};
 
-}
-
-//Calling the validate function. 
+//Calling the validate function.
 validate();
